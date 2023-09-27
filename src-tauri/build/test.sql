cf986913ca80c9834d5db93622825b7033cabd8b
Simeon Romanov <archeosanacritis@gmail.com>
2023-09-27 20:38:23 +0300

    sync

--- src-tauri/build/test.sql
+++ src-tauri/build/test.sql
@@ -1,5 +1,12 @@
-USE NS TuringApp;
-USE DB TuringDB;
+USE NS Dispatcher;
+USE DB DispatcherDB;
 
-SELECT * FROM user;
+INFO FOR DB;
 
+SELECT * FROM role;
+
+
+
+CREATE session:2;
+RELATE human:2->participateIn->session:2;
+SELECT id, <-participateIn<-human.name, <-participateIn<-interroagtor.name, <-participateIn<-computer.model, ->includes->answer.text, ->includes->question.text, ->includes->verdict.correct FROM session;
